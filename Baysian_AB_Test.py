8bf45c491a07ab33a44f0095ab606945d924184f
kota-mizu <kota.mizuno.en@gmail.com>
2024-11-11 09:39:41 +0900

    Update Baysian_AB_Test.py

--- Baysian_AB_Test.py
+++ Baysian_AB_Test.py
@@ -239,18 +239,10 @@ if st.session_state.authenticated:
         plt.title('Difference (B - A) Posterior Distribution')
         st.pyplot(fig2)
     
-    # トレースプロット
-    st.markdown('<h4>トレースプロット</h4>', unsafe_allow_html=True)
-
-    # プロット作成
-    fig, axes = plt.subplots(figsize=(10, 6))  # サイズを指定
-    pm.plot_trace(trace, compact=False, axes=axes)
-    
-    # レイアウト調整
-    plt.tight_layout()
-    
-    # Streamlitに表示
-    st.pyplot(fig)
+    fig3, axes = plt.subplots(2, 2, figsize=(15, 10))
+    pm.plot_trace(trace, var_names=['p_a', 'p_b'], axes=axes, compact=False)
+    plt.tight_layout()  # レイアウト調整
+    st.pyplot(fig3)
 
     
     # 統計的まとめ
